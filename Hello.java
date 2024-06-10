359c4a483e356004fcbc579505b62525adc6edec
danieloselu3 <doselu3@gmail.com>
2024-06-10 08:18:03 +0300

    learning how to get user input in java

---
+++ Hello.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+
+        System.out.println(name);
+
+        scanner.close();
+    }
+}
