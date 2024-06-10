05e62817803a07ff4e5daea5ca0f48eb5c6d737d
danieloselu3 <doselu3@gmail.com>
2024-06-10 07:22:57 +0300

    Wrote my first java code

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Daniel";
+        int age = 28;
+        System.out.println("Hello world! My name is " + name + " I am " + age + " years old.");
+    }
+}
