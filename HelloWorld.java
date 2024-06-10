bc90952f6491d0b3c44be5f639201f489c4e14b6
danieloselu3 <doselu3@gmail.com>
2024-06-10 07:50:38 +0300

    practice on string methods in java

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name = "Daniel Oselu";
+
+        // // check the length of a string
+        // System.out.println(name.length());
+
+        // // check if a variable is empty
+        // System.out.println(name.isEmpty());
+
+        // // convert a string to uppercase
+        // System.out.println(name.toUpperCase());
+
+        // // convert a string to lowercase
+        // System.out.println(name.toLowerCase());
+
+        // replace string
+        System.out.println(name.replace("Oselu", "Omondi"));
+    }
+}
